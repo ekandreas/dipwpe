a2c71c4e8b1af3a35acbc72deb98e526b842101d
Andreas Ek <andreas@andreasek.se>
2016-01-07 14:20:48 +0100

    Splitting pull database into several tasks

--- pull.php
+++ pull.php
@@ -1,48 +1,52 @@
 <?php
 
-task( 'pull:database', function () {
-
+task( 'pull:create_database_dump', function () {
     writeln( 'Creating a new database dump on the remote server' );
-    run( 'mysqldump {{remote.database}} > /tmp/{{remote.name}}.sql' );
-    writeln( 'Getting database dump from the remote server' );
-    runLocally( 'scp {{remote.ssh}}:/tmp/{{remote.name}}.sql {{remote.name}}.sql' );
-    writeln( 'Restore remote database to local database' );
-    runLocally( 'cd web && wp db import ../{{remote.name}}.sql' );
+    run( 'mysqldump {{remote.database}} > /tmp/{{remote.name}}.sql', 999 );
+} );
+
+task( 'pull:get_database_dump', function () {
+    writeln( 'Getting database dump from the remote server via scp' );
+    runLocally( 'scp {{remote.ssh}}:/tmp/{{remote.name}}.sql {{remote.name}}.sql', 999 );
+} );
+
+task( 'pull:restore_database', function () {
+    writeln( 'Restore remote database backup to local database' );
+    runLocally( 'cd web && wp db import ../{{remote.name}}.sql', 999 );
+} );
+
+task( 'pull:search_and_replace_database', function () {
     writeln( 'Search and replace urls in the imported database to local urls' );
     runLocally( 'cd web && wp search-replace www.{{remote.domain}} {{local.domain}}' );
     runLocally( 'cd web && wp search-replace {{remote.domain}} {{local.domain}}' );
-
 } );
 
 task( 'pull:files', function () {
-
     writeln( 'Getting uploads, long duration first time! (approx. 60s)' );
-    runLocally( 'rsync -re ssh {{remote.ssh}}:{{remote.path}}/shared/web/app/uploads web/app' );
-
+    runLocally( 'rsync -re ssh {{remote.ssh}}:{{remote.path}}/shared/web/app/uploads web/app', 999 );
 } );
 
 task( 'pull:elastic', function () {
-
     if( env('local.is_elastic') ) {
         writeln( 'Setup elasticsearch and elasticpress' );
-        runLocally( 'cd web && wp elasticpress index --setup' );
+        runLocally( 'cd web && wp elasticpress index --setup', 999 );
     }
-
 } );
 
 task( 'pull:cleanup', function () {
-
     writeln( 'Cleaning up locally...' );
     runLocally( 'rm {{remote.name}}.sql' );
     writeln( 'Permalinks rewrite/flush' );
     runLocally( 'cd web && wp rewrite flush' );
     writeln( 'Activate query monitor' );
     runLocally( 'cd web && wp plugin activate query-monitor' );
-
 } );
 
 task( 'pull', [
-    'pull:database',
+    'pull:create_database_dump',
+    'pull:get_database_dump',
+    'pull:restore_database',
+    'pull:search_and_replace_database',
     'pull:files',
     'pull:elastic',
     'pull:cleanup',
